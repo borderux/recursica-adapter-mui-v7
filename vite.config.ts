1fb83591996a495474368b53f5c84276413d7af5
Matt Massey <mattlmassey@gmail.com>
2026-08-11 08:22:02 -0700

    Fixed type export (#487)
    Co-authored-by: Matt Massey <matt@borderux.com>

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,8 @@ export default defineConfig(({ mode }) => {
             dts({
               insertTypesEntry: true,
               exclude: ["**/*.stories.*", ".storybook/**"],
+              rollupTypes: true,
+              bundledPackages: ["@recursica/adapter-common"],
             }),
           ]
         : []),
